20adb4bf66e00f9bc228a8802352c8eeb4f594ba
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-07-01 17:05:32 -0300

    Add project files.

---
+++ SomaDeImparesConsecutivosII_URI1099/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SomaDeImparesConsecutivosII_URI1099
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+
+                int x = int.Parse(vet[0]);
+                int y = int.Parse(vet[1]);                
+
+                if (x > y)
+                {
+                    int aux = x;
+                    x = y;
+                    y = aux;
+                }
+
+                int total = 0;
+                for (int j = x + 1; j < y; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        total = total + j;
+                    }
+                }
+
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
